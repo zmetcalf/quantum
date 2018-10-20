c69240342cfc0560a78e5102c3b0cf9dcacc8dc4
Zachery Metcalf <zachery.metcalf@gmail.com>
2018-10-20 11:13:16 -0600

    Initial SQL buildout

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-      mysql   = require('mysql');
+      mysql   = require('mysql'),
+      _       = require('lodash');
 
 const app = express();
 
@@ -14,7 +15,40 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/api/session/', (req, res) => {
   connection.connect();
-  res.json([ 'test' ]);
+
+  connection.query(
+    `SELECT * FROM `session` WHERE ${_.chain(req.params.filters)
+      .map(filter.operator => {
+          case 'equals':
+            return '`' + connection.escape(filter.column) + '`="' + connection.escape(filter.value) + '"';
+          case: 'contains':
+            return '`' + connection.escape(filter.column) + '` LIKE "%' + connection.escape(filter.value) + '%"';
+          case: 'starts with':
+            return '`' + connection.escape(filter.column) + '` LIKE "' + connection.escape(filter.value) + '%"';
+          case: 'in list':
+            return '`' + connection.escape(filter.column) + '` IN (' +
+              _.chain(filter.list)
+                .map(item => connection.escape(item))
+                .join(', ')
+                .value()
+            + ')';
+          case: 'between':
+            if(filter.list.length !== 2) return;
+            return '`' + connection.escape(filter.column) + '` BETWEEN ' + connection.escape(filter.list[0]) + ' AND ' +  connection.escape(filter.list[1]);
+          case 'greater than':
+            return '`' + connection.escape(filter.column) + '` > ' + connection.escape(filter.value);
+          case 'less than':
+            return '`' + connection.escape(filter.column) + '` < ' + connection.escape(filter.value);
+        }
+      })
+      .compact()
+      .join(' AND ')
+      .value()
+    })}`, (err, results, fields) => {
+      connection.end();
+      if(err) return res.status(500).json({ error: 'Query Could Not Be Processed' });
+      res.json(results);
+    });
 });
 
 app.listen('3000');
