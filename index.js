8696ccb12d444d13710d533ae6a40d7f980cb530
Zachery Metcalf <zachery.metcalf@gmail.com>
2018-10-20 11:54:13 -0600

    Rework MySQL after testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express'),
 
 const app = express();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host:     'db',
   user:     process.env.USER,
   password: process.env.PASSWORD,
@@ -13,42 +13,60 @@ const connection = mysql.createConnection({
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/api/session/', (req, res) => {
-  connection.connect();
+app.get('/api/session', (req, res) => {
+  if(!req.query.filters) return res.json([]);
 
-  connection.query(
-    `SELECT * FROM `session` WHERE ${_.chain(req.params.filters)
-      .map(filter.operator => {
-          case 'equals':
-            return '`' + connection.escape(filter.column) + '`="' + connection.escape(filter.value) + '"';
-          case: 'contains':
-            return '`' + connection.escape(filter.column) + '` LIKE "%' + connection.escape(filter.value) + '%"';
-          case: 'starts with':
-            return '`' + connection.escape(filter.column) + '` LIKE "' + connection.escape(filter.value) + '%"';
-          case: 'in list':
-            return '`' + connection.escape(filter.column) + '` IN (' +
-              _.chain(filter.list)
-                .map(item => connection.escape(item))
-                .join(', ')
-                .value()
-            + ')';
-          case: 'between':
-            if(filter.list.length !== 2) return;
-            return '`' + connection.escape(filter.column) + '` BETWEEN ' + connection.escape(filter.list[0]) + ' AND ' +  connection.escape(filter.list[1]);
-          case 'greater than':
-            return '`' + connection.escape(filter.column) + '` > ' + connection.escape(filter.value);
-          case 'less than':
-            return '`' + connection.escape(filter.column) + '` < ' + connection.escape(filter.value);
+  let filters = [];
+  try {
+    filters = JSON.parse(req.query.filters);
+  } catch(e) {
+    console.error(e);
+    return res.status(400).json('Could not parse request');
+  };
+
+  const query = 'SELECT * FROM `session` WHERE ' + _.chain(filters)
+    .map(filter => {
+      switch(filter.operator) {
+        case 'equals':
+          return mysql.escapeId(filter.column) + '=' + mysql.escape(filter.value);
+        case 'contains':
+          return mysql.escapeId(filter.column) + ' LIKE ' + mysql.escape(`%${filter.value}%`);
+        case 'starts with':
+          return mysql.escapeId(filter.column) + ' LIKE ' + mysql.escape(`${filter.value}%`);
+        case 'in list':
+          return mysql.escapeId(filter.column) + ' IN (' +
+            _.chain(filter.list)
+              .map(item => mysql.escape(item))
+              .join(', ')
+              .value()
+          + ')';
+        case 'between':
+          if(filter.list.length !== 2) return;
+          return mysql.escapeId(filter.column) + ' BETWEEN ' + mysql.escape(filter.list[0]) + ' AND ' +  mysql.escape(filter.list[1]);
+        case 'greater than':
+          return mysql.escapeId(filter.column) + ' > ' + mysql.escape(filter.value);
+        case 'less than':
+          return mysql.escapeId(filter.column) + ' < ' + mysql.escape(filter.value);
+      };
+    })
+    .compact()
+    .join(' AND ')
+    .value();
+
+  pool.getConnection((err, connection) => {
+    if(err) {
+      console.error(err);
+      res.status(500).json({ error: 'Server Error' });
+    }
+    connection.query(query, (err, results, fields) => {
+        connection.release();
+        if(err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Query Could Not Be Processed' });
         }
-      })
-      .compact()
-      .join(' AND ')
-      .value()
-    })}`, (err, results, fields) => {
-      connection.end();
-      if(err) return res.status(500).json({ error: 'Query Could Not Be Processed' });
-      res.json(results);
-    });
+        res.json(results);
+      });
+  });
 });
 
 app.listen('3000');
